1dc7687dbe8650ca8587e14b79381102caa2ca42
TDegimbe <thomas.degimbe@gmail.com>
2021-11-12 13:17:14 +0100

    Connexion
    connexion

--- src/app/components/connection-form/connection-form.component.ts
+++ src/app/components/connection-form/connection-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-connection-form',
@@ -7,8 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectionFormComponent implements OnInit {
 
-  constructor() { }
+  connectionForm: FormGroup;
+  public errors = {};
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder,private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onChange(input){
+    if(this.connectionForm.get(input).hasError('required')){
+      this.errors[input] = 'Ce champ est obligatoire';
+    }else{
+      delete this.errors[input];
+    }
+  }
+
+  onSubmit(){
+    const formValue = this.connectionForm.value;
+    this.authService.auth(formValue.user,formValue.password).then(status1 =>{
+      if(status1){
+        this.router.navigate(["/home"]);
+      }
+      else{
+        this.errors['password'] = "Le nom d'utilisateur ou le mot de passe est incorrect"
+      }
+
+    });
+  }
+
+  initForm() {
+    this.connectionForm = this.formBuilder.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
 }
