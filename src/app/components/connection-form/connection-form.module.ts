1354bcacd029fc6831ad37d5449dec9b6930dede
TDegimbe <thomas.degimbe@gmail.com>
2021-11-12 13:59:34 +0100

    suite du commit d'avant
    a

--- src/app/components/connection-form/connection-form.module.ts
+++ src/app/components/connection-form/connection-form.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import {ConnectionFormComponent} from './connection-form.component';
-
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   exports: [
     ConnectionFormComponent
